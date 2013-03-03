74e116d8c2e70bdd4b4b2689d2f5a9523da0eade
captain-caps <dont@want.this>
2013-03-03 19:25:01 +0100

    not working
    problems with creating intent

--- src/de/unidue/wiwi/tdr/kn3/rasp_home/RoomControl.java
+++ src/de/unidue/wiwi/tdr/kn3/rasp_home/RoomControl.java
@@ -1,11 +1,10 @@
 package de.unidue.wiwi.tdr.kn3.rasp_home;
 
 
-import java.util.List;
+
 
 import de.unidue.wiwi.tdr.kn3.rasp_home.CommunicationClass.RequestMessage.Method;
 import de.unidue.wiwi.tdr.kn3.rasp_home.CommunicationClass.RequestMessage.Type;
-import de.unidue.wiwi.tdr.kn3.rasp_home.DatabaseClass.Room;
 import android.app.ActionBar;
 import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
@@ -14,63 +13,66 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-//import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class RoomControl  extends Activity {
 
 	
 
-	String[] room = new String[1];
+	String[] room = null;
 
 	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
+		setContentView(R.layout.activity_roomcontrol);
+		Bundle extras = getIntent().getExtras();
+	    if (extras == null) {
+	      return;
+	    }
+	//	  Intent i = getIntent();
+	        // Receiving the Data
+	        room = extras.getStringArray("rooms");
+	        String curRoom = extras.getString("currentRoom");
+	        
 		room[0]="Kitchen";
-	//	CommunicationClass.ResponseMessage response = MainApplication.com.client.SendRequest(new CommunicationClass.RequestMessage(Method.GET, Type.Room, null, null, null, null));
+		CommunicationClass.ResponseMessage response = MainApplication.com.client.SendRequest(new CommunicationClass.RequestMessage(Method.GET, Type.Node, null, curRoom, null, null));
 		
 		//MainApplication.database.rooms.
 	//	MainApplication.database.rooms.
 	//room = response value
-		
+	//TODO
+		//Create Linear layout with nodes and button OR value depending on nodetype
 		
 		ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_dropdown_item, room);
-		
+		ActionBar.OnNavigationListener navigationListener = new OnNavigationListener() {
+			 
+            @Override
+            public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+                Toast.makeText(getBaseContext(), "You selected : " + room[itemPosition]  , Toast.LENGTH_SHORT).show();
+     Intent nextScreen = new Intent(getApplicationContext(), RoomControl.class);
+                
+                //Intent mit den Daten füllen
+                nextScreen.putExtra("Rooms", room);
+                nextScreen.putExtra("currentRoom", room[itemPosition]);
+ 
+               
+ 
+                // Intent starten und zur zweiten Activity wechseln
+                startActivity(nextScreen);
+                return true;
+            }
+        };
  
 		actionBar.setListNavigationCallbacks(adapter, navigationListener);
 	
 
 		}
 	
-	ActionBar.OnNavigationListener navigationListener = new OnNavigationListener() {
-		 
-        @Override
-        public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-        	
-            Toast.makeText(getBaseContext(), "You selected : " + room[itemPosition]  , Toast.LENGTH_SHORT).show();
-            
-  //   Intent nextScreen = new Intent(getApplicationContext(), RoomControl.class);
-            Intent nextScreen = new Intent(getApplicationContext(), RoomControl.class);
-            
-          //Intent mit den Daten füllen
-            nextScreen.putExtra("Rooms", room);
-            nextScreen.putExtra("currentRoom", room[itemPosition]);
-     
 
-            // Intent starten und zur zweiten Activity wechseln
-            startActivity(nextScreen);
-     
-            //TODO 
-            //true false whatever?
-            return true;
-        }
-    };
 		
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -83,7 +85,6 @@ public class MainActivity extends Activity {
 		switch (item.getItemId()) {
 		case R.id.menu_settings:
 			startActivity(new Intent(this, SettingsActivity.class));
-			
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
